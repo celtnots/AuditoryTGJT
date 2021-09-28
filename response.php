b55b90fcfd8ed76483571119a376bd48f5b6322e
celtnots <hunter@gonzaga.edu>
2021-09-28 12:54:20 +0200

    Update response.php

--- response.php
+++ response.php
@@ -18,7 +18,7 @@ $status = $decoded['status'];
 
 $Confidence = $decoded['Confidence'];
 
-$link = mysqli_connect("mysql.floatingcrowbar.com", "celtnots", "bridjam") or die ("didn't connect to mysql");
+$link = mysqli_connect("server", "username", "password") or die ("didn't connect to mysql");
 mysqli_select_db($link, "globalsmalltalk") or die(mysqli_error($link));
 mysqli_query($link, "INSERT INTO `errorrecognition` (`UserName`,`sentenceID`,`answer`,`elapsedTime`,`status`,`Confidence`,`score`) VALUES ('$UserName','$sentenceID','$answer','$elapsedTime','$status', '$Confidence', '$AnswerScore')");
 
